2a075f4bc88b314f6df950bd2102317821406dd4
Elias Augusto <elias.augusto@valtech.com>
2026-02-06 12:45:09 -0300

    index

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,3 +1,22 @@
 export default function decorate(block) {
-  block.classList.add('banner--decorated');
+  const img = block.querySelector('img');
+  const heading = block.querySelector('h1');
+  const text = block.querySelector('p');
+
+  block.innerHTML = '';
+
+  const wrapper = document.createElement('div');
+  wrapper.className = 'banner__inner';
+
+  const media = document.createElement('div');
+  media.className = 'banner__media';
+  if (img) media.append(img.closest('picture') || img);
+
+  const content = document.createElement('div');
+  content.className = 'banner__content';
+  if (heading) content.append(heading);
+  if (text) content.append(text);
+
+  wrapper.append(media, content);
+  block.append(wrapper);
 }
